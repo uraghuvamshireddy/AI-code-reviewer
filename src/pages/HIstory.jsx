b5e471b3a2f3a229fb23fd8ac66e120059f14eb2
uraghuvamshireddy <uraghuvamshireddy@gmail.com>
2025-04-21 12:04:04 +0530

    added delete file

--- src/pages/HIstory.jsx
+++ src/pages/HIstory.jsx
@@ -20,14 +20,35 @@ useEffect(()=>{
     fetchHistory();
 },[]);
 
+const deleteFile = async (id)=>{
+  try{
+    const token = localStorage.getItem('token');
+  const res = await axios.delete(`${import.meta.env.VITE_API_URL}/code/delete-file/${id}`,{
+    headers: {
+        Authorization: `Bearer ${token}`,
+      },
+});
+  alert('File deleted successflly')
+  setFiles(prev => prev.filter(file => file._id !== id));
+
+  }catch(err){
+    console.log(err);
+    alert('Error in deleting the file')
+  }
+}
+
 return (
   <div className="history-container">
     <h2>Saved Files</h2>
     <div className="file-grid">
       {files.map((file) => (
         <Link key={file._id} to={`/file/${file._id}`} className="file-card">
-          <h4>{file.filename}</h4>
+          <h2>{file.filename}</h2>
           <p>{new Date(file.createdAt).toLocaleString()}</p>
+          <button className='delete-file' onClick={(e) => {
+            e.preventDefault(); 
+            deleteFile(file._id);
+          }}>Delete</button>
         </Link>
       ))}
     </div>
